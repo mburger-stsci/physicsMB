bbf9109ffa70a0296db6b474680cd4fdf16c74d2
Matthew Burger <mburger@stsci.edu>
2019-04-22 16:26:40 -0400

    minor exception changes

--- physicsMB/atomicmass.py
+++ physicsMB/atomicmass.py
@@ -1,26 +1,37 @@
+"""Return the atomic mass of an atom or molecule.
+
+This is really just a wrapper for
+`periodictable
+<https://periodictable.readthedocs.io/en/latest/index.html>`_
+but returns the mass as an astropy quantity.
+"""
+
+import sys
 import periodictable as pt
 from periodictable import formulas
 import astropy.units as u
 
 
 def atomicmass(species):
-    '''
-    Returns the atomic mass of an atom or molecule.
+    """Return the atomic mass of an atom or molecule.
 
     This is really just a wrapper for
-    `periodictable <https://periodictable.readthedocs.io/en/latest/index.html>`_
+    `periodictable
+    <https://periodictable.readthedocs.io/en/latest/index.html>`_
     but returns the mass as an astropy quantity.
-    '''
-
+    """
     el = [e.symbol for e in pt.elements]
     if species in el:
-        atom = eval('pt.'+species)
+        atom = eval('pt.' + species)
         mass = atom.mass * u.u
     else:
         try:
             mass = formulas.formula(species).mass * u.u
-        except:
-            print(f'Species={species} not found')
+        except ValueError:
+            print(f'WARNING: mathMB.atomicmass: {species} not found')
             mass = None
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+            raise
 
     return mass
